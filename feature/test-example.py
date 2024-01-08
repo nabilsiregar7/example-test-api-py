34710f132efd500f7aebec33cda8b2c888f06ece
nabilsiregar7 <nsiregar7@gmail.com>
2024-01-08 16:27:31 +0700

    add new api test to create and retrieve user

---
+++ feature/test-example.py
@@ -0,0 +1,17 @@
+import requests
+import pytest
+
+API_URL = 'https://reqres.in/api/users'
+
+def test_create_user():
+    #Test case to create new user
+    data = {'name': 'John Doe', 'job': 'Engineer'}
+    response = requests.post(API_URL, json=data)
+    assert response.status_code == 201
+    assert 'id' in response.json()
+
+def test_get_user_list():
+    #Test case to retrieve list user
+    response = requests.get(API_URL)
+    assert response.status_code == 200
+    assert 'data' in response.json()
